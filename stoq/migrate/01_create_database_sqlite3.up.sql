28919593dcfc9f72ad362cf245f59068eea76cd5
Micael Rocha <micaelrrocha13@gmail.com>
2022-12-07 15:07:21 -0300

    Arquivo 'migrate' Atualizado

--- stoq/migrate/01_create_database_sqlite3.up.sql
+++ stoq/migrate/01_create_database_sqlite3.up.sql
@@ -12,3 +12,17 @@ CREATE TABLE sqlite_sequence(name,seq);
 -- Criando trigger
 CREATE TRIGGER tb_product_updated_at_trig AFTER UPDATE OF name, code, price ON tb_product FOR EACH ROW BEGIN UPDATE tb_product SET updated_at = datetime('now', 'localtime') WHERE id = NEW.id; 
 END;
+
+INSERT INTO tb_product (name_product, code, price) VALUES 
+    ('Notebook 13 XPS', 'DELL', 14500),
+    ('Notebook 15', 'DELL', 2500),
+    ('Notebook 14', 'DELL', 2500),
+    ('Notebook 15', 'Lenovo', 3500),
+    ('Notebook 13', 'Lenovo', 4568),
+    ('Tablet', 'SAM', 8450),
+    ('Macbook 13 Pro M1', 'Apple', 18500.00),
+    ('TV 55', 'SONY', 4500.00),
+    ('TV 45', 'SONY', 3500.00),
+    ('TV 32', 'SAM', 2500.00),
+    ('TV 60', 'LG', 6500.00),
+    ('TV 50', 'LG', 4800.00);
